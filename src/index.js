a58e86ff6ab53001490ec41f341c56368356faba
Franco Rossi <frossi@sofrecom.com.ar>
2019-10-15 11:56:18 -0300

    database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+
+const { mongoose } = require('./database');
+
 const app = express();
 
 // SETTINGS
@@ -10,9 +14,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 // ROUTES
-app.use(require('./routes/task.routes'));
+app.use('/api/tasks', require('./routes/task.routes'));
 
 // STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
 
 // SERVER START
 app.listen(app.get('port'), () => {
