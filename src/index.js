d2e3815a72340077900ae15d1fb067a647d08cec
Franco Rossi <frossi@sofrecom.com.ar>
2019-10-15 11:28:49 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // SETTINGS
+app.set('port', process.env.PORT || 3000);
 
 // MIDDLEWARES
+app.use(morgan('dev'));
+app.use(express.json());
 
 // ROUTES
 
 // STATIC FILES
 
 // SERVER START
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
