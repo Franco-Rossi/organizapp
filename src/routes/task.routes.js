7856f982c2f238670a9b2315fdff410eb9022924
Franco Rossi <frossi@sofrecom.com.ar>
2019-10-15 12:23:06 -0300

    post

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const Task = require('../models/task');
 
-router.get('/', (req, res) => {
-    Task.find(function(err, tasks) {
-        console.log(tasks);
-    });
-    res.json({
-        status: 'API Works'
-    });
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+router.post('/', async (req, res) => {
+    const { title, description } = req.body;
+    const task = new Task({ title, description });
+    await task.save();
+    res.json({ status: 'Task saved' });
 });
 
 module.exports = router;
