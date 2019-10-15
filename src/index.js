8bc509b2b0404e36bbab184ffb6c9bacb0922544
Franco Rossi <frossi@sofrecom.com.ar>
2019-10-15 11:15:21 -0300

    first upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// SETTINGS
+
+// MIDDLEWARES
+
+// ROUTES
+
+// STATIC FILES
+
+// SERVER START
+app.listen(3000, () => {
+    console.log('Server on port 3000');
+});
